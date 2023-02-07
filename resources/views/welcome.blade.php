c942f63dc64736f9d21ec6ac6c1870b464b5cb42
Lost2257 <aivaras735@gmail.com>
2023-02-07 18:03:17 +0200

    laravel9_vue3_crud

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel-vue-crud</title>
+        <script src="https://kit.fontawesome.com/069ab44bd6.js" crossorigin="anonymous"></script>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        @vite(['resources/js/app.js', 'resources/css/app.css'])
+    </head>
+    <body>
+        <div id="app">
+        
+        </div>
+    </body>
+</html>
